af84cd5eea8be43b80cbb0e7657518aa9ebf0fe2
Sabry <sabrytkdfox@outlook.com>
2020-09-25 20:35:05 +0200

    25/9/2020 adding favorites model and adapter lYOUT

--- app/src/main/java/com/example/hanakol_2ah/adapters/FavoritesAdapter.java
+++ app/src/main/java/com/example/hanakol_2ah/adapters/FavoritesAdapter.java
@@ -1,4 +1,70 @@
 package com.example.hanakol_2ah.adapters;
 
-public class FavoritesAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import com.example.hanakol_2ah.R;
+import com.example.hanakol_2ah.models.Favorites;
+import com.example.hanakol_2ah.models.Meals;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class FavoritesAdapter extends RecyclerView.Adapter<FavoritesAdapter.favoritesViewHolder> {
+
+    private ArrayList<Favorites> favoritesMealsList;
+    Context context;
+
+    public FavoritesAdapter(Context context,ArrayList<Favorites> favoritesMealsList) {
+
+        this.favoritesMealsList = favoritesMealsList;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public FavoritesAdapter.favoritesViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_view_pager_header_shimmer, parent, false);
+        return new FavoritesAdapter.favoritesViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FavoritesAdapter.favoritesViewHolder holder, int position) {
+        Favorites favorites = favoritesMealsList.get(position);
+        holder.favoritesName.setText(favorites.getFavoritesMealName());
+        holder.favoritesIngredients.setText(favorites.getFavoritesDescription());
+        holder.favoritesSteps.setText(favorites.getFavoritesSteps());
+        holder.favoritesMeal_rating_Bar.setRating(favorites.getFavoritesMealRate());
+        Picasso.get().load(favorites.getFavoritesImageURL()).into(holder.favoritesMealImageView);
+    }
+
+    @Override
+    public int getItemCount() {
+        return favoritesMealsList.size();
+    }
+
+    public class favoritesViewHolder extends RecyclerView.ViewHolder {
+        private TextView favoritesName , favoritesIngredients, favoritesSteps;
+        private ImageView favoritesMealImageView;
+        private RatingBar favoritesMeal_rating_Bar;
+
+        public favoritesViewHolder(View itemView) {
+            super(itemView);
+            favoritesMealImageView = itemView.findViewById(R.id.favorites_mealImageView);
+            favoritesName = itemView.findViewById(R.id.favorites_meal_name_item_recycler);
+            favoritesIngredients = itemView.findViewById(R.id.favorites_meal_ingredients_item_recycler);
+            favoritesSteps = itemView.findViewById(R.id.favorites_meal_steps_item_recycler);
+            favoritesMeal_rating_Bar = itemView.findViewById(R.id.favorites_meal_rating_Bar);
+
+        }
+    }
 }
--- app/src/main/java/com/example/hanakol_2ah/models/Favorites.java
+++ app/src/main/java/com/example/hanakol_2ah/models/Favorites.java
@@ -1,4 +1,58 @@
 package com.example.hanakol_2ah.models;
 
 public class Favorites {
+    private String favoritesMealName;
+    private String favoritesDescription;
+    private String favoritesSteps;
+    private String favoritesImageURL;
+
+    public String getFavoritesMealName() {
+        return favoritesMealName;
+    }
+
+    public void setFavoritesMealName(String favoritesMealName) {
+        this.favoritesMealName = favoritesMealName;
+    }
+
+    public String getFavoritesDescription() {
+        return favoritesDescription;
+    }
+
+    public void setFavoritesDescription(String favoritesDescription) {
+        this.favoritesDescription = favoritesDescription;
+    }
+
+    public String getFavoritesSteps() {
+        return favoritesSteps;
+    }
+
+    public void setFavoritesSteps(String favoritesSteps) {
+        this.favoritesSteps = favoritesSteps;
+    }
+
+    public String getFavoritesImageURL() {
+        return favoritesImageURL;
+    }
+
+    public void setFavoritesImageURL(String favoritesImageURL) {
+        this.favoritesImageURL = favoritesImageURL;
+    }
+
+    public Float getFavoritesMealRate() {
+        return favoritesMealRate;
+    }
+
+    public void setFavoritesMealRate(Float favoritesMealRate) {
+        this.favoritesMealRate = favoritesMealRate;
+    }
+
+    public Favorites(String favoritesMealName, String favoritesDescription, String favoritesSteps, String favoritesImageURL, Float favoritesMealRate) {
+        this.favoritesMealName = favoritesMealName;
+        this.favoritesDescription = favoritesDescription;
+        this.favoritesSteps = favoritesSteps;
+        this.favoritesImageURL = favoritesImageURL;
+        this.favoritesMealRate = favoritesMealRate;
+    }
+
+    private Float favoritesMealRate;
 }
